38f991b2e15a9618faaa78890664296f83f9f0f5
Marcel <izuobasomtochukwu@gmail.com>
2025-06-09 13:00:17 +0100

    update

--- music-player/src/profile.tsx
+++ music-player/src/profile.tsx
@@ -3,7 +3,7 @@ function Profile() {
   return (
   <>
    <div>
-      <img src={profile} alt="Character" style={{width: "12rem"}}/>
+      <img src={profile} alt="Character" style={{width: "12rem"}} loading='lazy'/>
    </div>
   </> 
   )
