d11ba11f1655c372760bd79d80122930f28c6ac0
Marcel <izuobasomtochukwu@gmail.com>
2025-07-07 22:46:18 +0100

    test

--- music-player/src/App.tsx
+++ music-player/src/App.tsx
@@ -1,23 +1,23 @@
-/* import { Routes,Route } from 'react-router-dom'; */
+import { Routes,Route } from 'react-router-dom';
 import Entrance from './Entrance';
-/* import MusicApp from './MusicApp';
+import MusicApp from './MusicApp';
 import AuthApp from './AuthPage';
+import Redirect from './Redirect';
 import SignIn from './signIn';
-import ProfileApp from './ProfileApp'; */
+import ProfileApp from './ProfileApp';
 function App() {
 
  
   return (
  <>
-{/*  <Routes>
+ <Routes>
      <Route path='/' element={<Redirect/>} />
     <Route path='/intro' element={<Entrance/>}/>
      <Route path='/auth' element={<AuthApp/>}/> 
     <Route path='/sign' element={<SignIn />}/>
     <Route path='/profile-setup' element={<ProfileApp/>}></Route>
     <Route path='/Main' element={<MusicApp/>}/>
- </Routes> */}
-   <Entrance/>
+  </Routes>
  </>
   );
 }
