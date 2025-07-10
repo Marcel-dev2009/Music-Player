9cfa7b2167f57efe58d22bd30fdbda7a373692b6
Marcel <izuobasomtochukwu@gmail.com>
2025-07-10 12:17:57 +0100

    improved redirect logic

--- music-player/src/Redirect.tsx
+++ music-player/src/Redirect.tsx
@@ -1,18 +1,28 @@
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
+
 const Redirect = () => {
    const navigate = useNavigate();
 
   useEffect(() => {
      const hasVisited = localStorage.getItem('hasVisited');
-      if(hasVisited){
+     const hasAuth = localStorage.getItem('hasAuth');
+
+     if(hasVisited){
     navigate('auth');
   }else{
     localStorage.setItem('hasVisited' , 'true');
     navigate('/intro');
   }
+    if(hasAuth){
+      navigate('/profile-setup');
+    } else {
+      localStorage.setItem('hasAuth' , 'true');
+      navigate('/sign');
+    }
   }, [navigate]);
+   
   return null
 }
 
